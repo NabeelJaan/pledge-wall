2c8f75a7f02e6bf27fd523924070f04fa2a6cc8e
Tayyab Rehman <47273004+NabeelJaan@users.noreply.github.com>
2024-06-14 07:39:10 +0500

    style issue fixed.

--- pledge-wall.php
+++ pledge-wall.php
@@ -8,7 +8,7 @@
 
 <main>
     <!-- pledge Head -->
-    <div class="bg-red-600 text-white py-48">
+    <div class="bg-red-600 text-white py-28">
         <div class="max-w-[1200px] mx-auto">
             <div class="max-w-[600px] ml-auto">
                 <h1 class="text-3xl font-bold">Apex Pledge Wall</h1>
@@ -17,10 +17,10 @@
         </div>
     </div>
     <!-- Pledge -->
-    <div class="max-w-[1280px] mx-auto mt-8">
+    <div class="max-w-[1280px] mx-auto mt-4 pb-10">
         <div class="grid grid-cols-5 gap-3">
             <div class="w-full -mt-44 col-span-2">
-                <div class="bg-white p-3 border-2 border-red-600">
+                <div class="bg-white h-[479px] p-3 border-2 border-red-600">
                     <div class="flex justify-between items-center mb-4">
                         <h2 class="text-red-600 font-bold">Full Name</h2>
                         <img class="w-8 h-auto" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Country Flag">
@@ -29,7 +29,7 @@
                     <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
                     
                     <div class="mt-4">
-                        <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat, erat et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
                     </div>
                 </div>
             </div>
@@ -43,7 +43,7 @@
                     <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
                     
                     <div class="mt-4">
-                        <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat, erat et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
                     </div>
                 </div>
             </div>
@@ -58,7 +58,7 @@
                     <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
                     
                     <div class="mt-4">
-                        <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat, erat et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
                     </div>
                 </div>
             </div>
@@ -72,7 +72,7 @@
                     <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
                     
                     <div class="mt-4">
-                        <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat, erat et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
                     </div>
                 </div>
             </div>
@@ -86,7 +86,63 @@
                     <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
                     
                     <div class="mt-4">
-                        <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat, erat et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full">
+                <div class="bg-white p-3 border-2 border-red-600">
+                    <div class="flex justify-between items-center mb-4">
+                        <h2 class="text-red-600 font-bold">Full Name</h2>
+                        <img class="w-8 h-auto" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Country Flag">
+                    </div>
+
+                    <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
+                    
+                    <div class="mt-4">
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full">
+                <div class="bg-white p-3 border-2 border-red-600">
+                    <div class="flex justify-between items-center mb-4">
+                        <h2 class="text-red-600 font-bold">Full Name</h2>
+                        <img class="w-8 h-auto" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Country Flag">
+                    </div>
+
+                    <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
+                    
+                    <div class="mt-4">
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full">
+                <div class="bg-white p-3 border-2 border-red-600">
+                    <div class="flex justify-between items-center mb-4">
+                        <h2 class="text-red-600 font-bold">Full Name</h2>
+                        <img class="w-8 h-auto" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Country Flag">
+                    </div>
+
+                    <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
+                    
+                    <div class="mt-4">
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full">
+                <div class="bg-white p-3 border-2 border-red-600">
+                    <div class="flex justify-between items-center mb-4">
+                        <h2 class="text-red-600 font-bold">Full Name</h2>
+                        <img class="w-8 h-auto" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Country Flag">
+                    </div>
+
+                    <img class="w-full h-auto object-cover" src="/wp-content/uploads/2024/06/womenjpg.jpg" alt="Profile Picture">
+                    
+                    <div class="mt-4">
+                        <p class="mt-2 text-gray-600">et pulvinar interdum, lacus enim vehicula erat, in rutrum turpis justo non ex. Etiam vehicula est ante id bibendum.</p>
                     </div>
                 </div>
             </div>
